e70e6489fa82bb9e1ba7662f0b223c0fda58be19
Vinicius Kobs <viniciusrehbeinkobs@gmail.com>
2022-08-27 13:32:27 -0300

    requisito 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
+
+const talkerPath = path.join(__dirname, './talker.json');
+
+const readTalkerData = async () => {
+  try {
+    const data = await fs.readFile(talkerPath, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+};
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,3 +28,5 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+app.get('/talker', async (req, res) => res.status(200).json(await readTalkerData()));
